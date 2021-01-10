5d8c64c5858c42209f9221513fccd9a55450ecb3
Asjid Kalam <asjid.kalam@gmail.com>
2021-01-10 15:59:18 +0530

    fixed prototype pollution bug

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, '__esModule', {
 
 exports.default = exports.expand = (obj, opts = {}) => {
   return Object.keys(obj).reduce((a, c) => {
+    if(c.includes('__proto__') || c.includes('constructor') || c.includes('prototype')){
+      return a;
+    }
     set(a, c, obj[c])
     return a
   }, {})
